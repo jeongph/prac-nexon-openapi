2a1dd98a5544f9f42d571a43bafe8fa9435332ae
JeongUk Park <dnr1105@gmail.com>
2024-01-03 23:31:03 +0900

    init: initialize

---
+++ src/main/java/me/jeongph/pracnexonopenapi/PracNexonOpenapiApplication.java
@@ -0,0 +1,13 @@
+package me.jeongph.pracnexonopenapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracNexonOpenapiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PracNexonOpenapiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/me/jeongph/pracnexonopenapi/PracNexonOpenapiApplicationTests.java
@@ -0,0 +1,13 @@
+package me.jeongph.pracnexonopenapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracNexonOpenapiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
